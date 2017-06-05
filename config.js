ac8e857aa8d1f67f1eab522c3a932315c37a6e42
knarfeh <knarfeh@outlook.com>
2017-06-05 22:47:55 +0800

    Update

---
+++ config.js
@@ -0,0 +1,5 @@
+/**
+ * config
+ */
+
+var path = require('path');
---
+++ controllers/sign.js
@@ -0,0 +1,17 @@
+var validator = require('validator');
+
+
+// sign up
+exports.showSignup = function(req, res) {
+    res.render('sign/signup')
+};
+
+exports.signup = function(req, res, next) {
+    var login_name = validator.trim(req.body.login_name).toLowerCase();
+    var email = validator.trim(req.body.email).toLowerCase();
+    var password = validator.trim(req.body.pass);
+    var re_pass = validator.trim(req.body.re_pas)
+
+    // Verify the correctness of the registration information
+    // TODO
+}
