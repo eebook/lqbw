4c86a2c4eae7063cd6dfd925ad94885bf933a5d3
knarfeh <knarfeh@outlook.com>
2017-07-26 23:34:51 +0800

    feat: Project skeleton, add log4js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+/**
+ * eebook - app.js
+ */
+
+var config = require('./config')
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var logger = require('./common/logger');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -15,8 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -24,6 +29,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+var urlinfo = require('url').parse(config.host);
+config.hostname = urlinfo.hostname || config.host;
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,5 +50,14 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+if (!module.parent) {
+  app.listen(config.port, function () {
+    logger.info('eebook listening on port', config.port);
+    logger.info('May the force be with you...');
+    logger.info('You can debug your app with http://' + config.hostname + ':' + config.port);
+  });
+}
+
 
 module.exports = app;
