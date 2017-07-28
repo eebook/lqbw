677eed760a28de3f5100f0d5953643ed59901481
knarfeh <knarfeh@outlook.com>
2017-07-28 23:42:38 +0800

    chron: Refactoring

--- config.default.ts
+++ config.default.ts
@@ -1,31 +1,44 @@
 /**
- * config
+ * config for eebook.org
+ * TODO: load some sensitive environment variables from env file
  */
 
- var path = require('path');
+const PATH = require('path');
+const EEBOOK_COMPONENT = 'eebookorg'
 
- var config = {
-     debug: true,
+const config = {
+    DEBUG: true,
 
-     // The port that the program runs.
-     port: 3001,
-     // Domain name
-     host: 'localhost',
+    SESSIONCONF: {
+        KEY: 'eebook:sess', /** (string) cookie key (default: eebook:sess */
+        MAXAGE: 86400000, /** (number) maxAge in ms (default: 1 day) */
+        OVERWRITE: true, /** (boolean) can overWrite or not (default: true) */
+        HTTPONLY: true, /** (boolean) httpOnly or not (default: true) */
+    },
 
-     // config for github.
-     GITHUB_OAUTH: {
-         clientID: 'GITHUB_CLIENT_ID',
-         clientSecret: 'GITHUB_CLIENT_SECRET',
-         callbackURL: 'http://localhost:3001/auth/github/callback'
-     },
+    STATICCONF: __dirname + '/public',
 
-     API_SERVER_URL: 'http://localhost:8082/',
-     API_VERSION: 'v1',
+    // The port that the program runs.
+    PORT: 3001,
+    // Domain name
+    HOST: 'localhost',
 
-     log_dir: path.join(__dirname, 'logs'),
+    // config for github.
+    GITHUB_OAUTH: {
+        clientID: 'GITHUB_CLIENT_ID',
+        clientSecret: 'GITHUB_CLIENT_SECRET',
+        callbackURL: 'http://localhost:3001/auth/github/callback'
+    },
 
-     // Otherwise, you can only login with github.
-     allow_sign_up: true,
- }
+    API_SERVER_URL: 'http://localhost:8082/',
+    API_VERSION: 'v1',
 
- module.exports = config;
+    LOGGERCONFIG:  {
+        LOGDIR: PATH.join(__dirname, EEBOOK_COMPONENT)
+    },
+
+    // Otherwise, you can only login with github.
+    ALLOW_SIGN_UP: true,
+}
+
+export default config;
--- server/app.ts
+++ server/app.ts
@@ -3,15 +3,15 @@
  */
 
 
-import * as express from "express";
-import * as path from "path";
-import * as cookieParser from "cookie-parser";
-import * as bodyParser from "body-parser";
+import * as express from 'express';
+import * as path from 'path';
+import * as cookieParser from 'cookie-parser';
+import * as bodyParser from 'body-parser';
 
-import * as logger from "./common/logger";
-import * as config from "../config";
-import * as index from "./routes/index";
-import * as users from "./routes/users";
+import config from '../config';
+import eebLogger from './logger/logger';
+import * as index from './routes/index';
+import * as users from './routes/users';
 
 
 const app = express();
@@ -19,9 +19,10 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
-app.engine('html', require)
+app.engine('html', require('ejs').renderFile);
 
 // uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -30,34 +31,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
-var urlinfo = require('url').parse(config.host);
-config.hostname = urlinfo.hostname || config.host;
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+  // var err = new Error('Not Found');
+  // err.status = 404;
+  // next(err);
+// });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 if (!module.parent) {
-  app.listen(config.port, function () {
-    logger.info('eebook listening on port', config.port);
-    logger.info('May the force be with you...');
-    logger.info('You can debug your app with http://' + config.hostname + ':' + config.port);
+  app.listen(config.PORT, function () {
+    eebLogger.logger.info('eebook listening on port', config.PORT);
+    eebLogger.logger.info('May the force be with you...');
+    eebLogger.logger.info('You can debug your app with http://' + config.HOST + ':' + config.PORT);
   });
 }
 
-
-module.exports = app;
+export { app };
