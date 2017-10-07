d13c7a3c220e567506da6044694d0623ae318089
knarfeh <knarfeh@outlook.com>
2017-10-07 21:16:31 +0800

    feat: Rewrite job config list table with covalent

--- client/app/job/config/job-config-list/job-config-list.component.ts
+++ client/app/job/config/job-config-list/job-config-list.component.ts
@@ -1,204 +1,24 @@
+import { Component, OnInit } from '@angular/core';
 import { TdDataTableService, TdDataTableSortingOrder, ITdDataTableSortChangeEvent, ITdDataTableColumn } from '@covalent/core';
-import { IPageChangeEvent } from '@covalent/core';
-
-const NUMBER_FORMAT: (v: any) => any = (v: number) => v;
-const DECIMAL_FORMAT: (v: any) => any = (v: number) => v.toFixed(2);
 
 @Component({
   selector: 'app-job-config-list',
   templateUrl: './job-config-list.component.html',
   styleUrls: ['./job-config-list.component.scss'],
-  providers: [
-  ]
+
 })
 
-export class JobConfigListComponent implements OnInit {
-  columns: ITdDataTableColumn[] = [
-    { name: 'name',  label: 'Dessert (100g serving)', sortable: true, width: 200 },
-    { name: 'type', label: 'Type', filter: true },
-    { name: 'calories', label: 'Calories', numeric: true, format: NUMBER_FORMAT, sortable: true, hidden: false },
-    { name: 'fat', label: 'Fat (g)', numeric: true, format: DECIMAL_FORMAT, sortable: true },
-    { name: 'carbs', label: 'Carbs (g)', numeric: true, format: NUMBER_FORMAT },
-    { name: 'protein', label: 'Protein (g)', numeric: true, format: DECIMAL_FORMAT },
-    { name: 'sodium', label: 'Sodium (mg)', numeric: true, format: NUMBER_FORMAT },
-    { name: 'calcium', label: 'Calcium (%)', numeric: true, format: NUMBER_FORMAT },
-    { name: 'iron', label: 'Iron (%)', numeric: true, format: NUMBER_FORMAT },
-  ];
-
+export class JobConfigListComponent {
   data: any[] = [
-    {
-      'id': 1,
-      'name': 'Frozen yogurt',
-      'type': 'Ice cream',
-      'calories': 159.0,
-      'fat': 6.0,
-      'carbs': 24.0,
-      'protein': 4.0,
-      'sodium': 87.0,
-      'calcium': 14.0,
-      'iron': 1.0,
-      'comments': 'I love froyo!',
-    }, {
-      'id': 2,
-      'name': 'Ice cream sandwich',
-      'type': 'Ice cream',
-      'calories': 237.0,
-      'fat': 9.0,
-      'carbs': 37.0,
-      'protein': 4.3,
-      'sodium': 129.0,
-      'calcium': 8.0,
-      'iron': 1.0,
-    }, {
-      'id': 3,
-      'name': 'Eclair',
-      'type': 'Pastry',
-      'calories':  262.0,
-      'fat': 16.0,
-      'carbs': 24.0,
-      'protein':  6.0,
-      'sodium': 337.0,
-      'calcium':  6.0,
-      'iron': 7.0,
-    }, {
-      'id': 4,
-      'name': 'Cupcake',
-      'type': 'Pastry',
-      'calories':  305.0,
-      'fat': 3.7,
-      'carbs': 67.0,
-      'protein': 4.3,
-      'sodium': 413.0,
-      'calcium': 3.0,
-      'iron': 8.0,
-    }, {
-      'id': 5,
-      'name': 'Jelly bean',
-      'type': 'Candy',
-      'calories':  375.0,
-      'fat': 0.0,
-      'carbs': 94.0,
-      'protein': 0.0,
-      'sodium': 50.0,
-      'calcium': 0.0,
-      'iron': 0.0,
-    }, {
-      'id': 6,
-      'name': 'Lollipop',
-      'type': 'Candy',
-      'calories': 392.0,
-      'fat': 0.2,
-      'carbs': 98.0,
-      'protein': 0.0,
-      'sodium': 38.0,
-      'calcium': 0.0,
-      'iron': 2.0,
-    }, {
-      'id': 7,
-      'name': 'Honeycomb',
-      'type': 'Other',
-      'calories': 408.0,
-      'fat': 3.2,
-      'carbs': 87.0,
-      'protein': 6.5,
-      'sodium': 562.0,
-      'calcium': 0.0,
-      'iron': 45.0,
-    }, {
-      'id': 8,
-      'name': 'Donut',
-      'type': 'Pastry',
-      'calories': 452.0,
-      'fat': 25.0,
-      'carbs': 51.0,
-      'protein': 4.9,
-      'sodium': 326.0,
-      'calcium': 2.0,
-      'iron': 22.0,
-    }, {
-      'id': 9,
-      'name': 'KitKat',
-      'type': 'Candy',
-      'calories': 518.0,
-      'fat': 26.0,
-      'carbs': 65.0,
-      'protein': 7.0,
-      'sodium': 54.0,
-      'calcium': 12.0,
-      'iron': 6.0,
-    }, {
-      'id': 10,
-      'name': 'Chocolate',
-      'type': 'Candy',
-      'calories': 518.0,
-      'fat': 26.0,
-      'carbs': 65.0,
-      'protein': 7.0,
-      'sodium': 54.0,
-      'calcium': 12.0,
-      'iron': 6.0,
-    }, {
-      'id': 11,
-      'name': 'Chamoy',
-      'type': 'Candy',
-      'calories': 518.0,
-      'fat': 26.0,
-      'carbs': 65.0,
-      'protein': 7.0,
-      'sodium': 54.0,
-      'calcium': 12.0,
-      'iron': 6.0,
-    },
+    { sku: '1452-2', item: 'Pork Chops', price: 32.11 },
+    { sku: '1421-0', item: 'Prime Rib', price: 41.15 },
   ];
-
-  filteredData: any[] = this.data;
-  filteredTotal: number = this.data.length;
-
-  searchTerm: string = '';
-  fromRow: number = 1;
-  currentPage: number = 1;
-  pageSize: number = 10;
-  sortBy: string = 'name';
-  selectedRows: any[] = [];
-  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
-
-  constructor(private _dataTableService: TdDataTableService) {}
-
-  ngOnInit(): void {
-    this.filter();
-  }
-
-  sort(sortEvent: ITdDataTableSortChangeEvent): void {
-    this.sortBy = sortEvent.name;
-    this.sortOrder = sortEvent.order;
-    this.filter();
-  }
-
-  search(searchTerm: string): void {
-    this.searchTerm = searchTerm;
-    this.filter();
-  }
-
-  page(pagingEvent: IPageChangeEvent): void {
-    this.fromRow = pagingEvent.fromRow;
-    this.currentPage = pagingEvent.page;
-    this.pageSize = pagingEvent.pageSize;
-    this.filter();
-  }
-
-  filter(): void {
-    let newData: any[] = this.data;
-    const excludedColumns: string[] = this.columns
-    .filter((column: ITdDataTableColumn) => {
-      return ((column.filter === undefined && column.hidden === true) ||
-              (column.filter !== undefined && column.filter === false));
-    }).map((column: ITdDataTableColumn) => {
-      return column.name;
-    });
-    newData = this._dataTableService.filterData(newData, this.searchTerm, true, excludedColumns);
-    this.filteredTotal = newData.length;
-    newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
-    newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
-    this.filteredData = newData;
+  columns: ITdDataTableColumn[] = [
+    { name: 'sku', label: 'SKU #', tooltip: 'Stock Keeping Unit', sortable: true },
+    { name: 'item', label: 'Item name', width: 200 },
+    { name: 'price', label: 'Price (US$)', numeric: true, format: v => v.toFixed(2), width: { min: 100, max: 400 } },
+  ];
+  compareWith(row: any, model: any): boolean {
+    return row.id === model.id; // or any property you want to compare by.
   }
 }
